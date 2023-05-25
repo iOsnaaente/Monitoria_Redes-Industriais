c1120e789ea6051582b881bd9aa3bd5aff17edcf
Bruno Sampaio <bruno.bielsam.1205@hotmail.com>
2023-05-25 08:36:52 -0300

    Update Fred 25/05/23

--- Exemplo3 - Transferencia de arquivos/repositorio.py
+++ Exemplo3 - Transferencia de arquivos/repositorio.py
@@ -2,11 +2,13 @@ import threading
 import socket
 import os 
 
+buffer_size = 1024
+
 # Cria um socket TCP/IP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Define o endereço e a porta que o socket vai se vincular
-IP, PORT = 'localhost', 8000
+IP, PORT = '0.0.0.0', 8000
 print(f'Iniciando servidor em {IP}:{PORT}')
 server_socket.bind((IP, PORT))
 
@@ -16,18 +18,22 @@ server_socket.listen(5)
 # Caminho absoluto para o diretório das imagens
 files_path = os.path.dirname( __file__ ) + '/files/'
 
+print(files_path)
+
 # Pega os arquivos disponíveis na pasta files 
 files = os.listdir(files_path)
 # Adiciona um marcador de fim de lista End Of File 
-files.append( 'EOF')
-print( 'Arquivos disponíveis: ', files )
+print( 'Arquivos disponíveis: ')
+arquivos = ""
+for file in files:
+    arquivos += file + "\n";
+print(arquivos)
 
 # Função para tratar a comunicação com cada cliente paralelamente 
 def receive_file( client_socket ):
 
     # Envia os nomes dos arquivos disponíveis 
-    for file in files:
-        client_socket.send( file.encode() )
+    client_socket.send( arquivos.encode() )
 
     # Recebe a mensagem do cliente
     data = client_socket.recv(1024)
@@ -42,17 +48,30 @@ def receive_file( client_socket ):
         # Verifica se o arquivo existe
         if file_name in files:
             print(f'Arquivo {file_name} encontrado.')
+            file_stats = os.stat(files_path + file_name)
+            #print(file_stats)
+            print(f'File Size in Bytes is {file_stats.st_size}')
+            print(f'File Size is %.1f kB' % (file_stats.st_size / (1024)))
+            #print(f'File Size in MegaBytes is {file_stats.st_size / (1024 * 1024)}')
+
+            data_size = 0
+            acum_data_transfer = 0
+
             # Abre o arquivo para leitura
             with open( files_path + file_name, 'rb') as f:
                 # Envia o arquivo em pacotes de 1024 em 1024 bytes
                 count = 0
                 while True:
-                    data = f.read(15000)
+                    data = f.read(buffer_size)
                     if not data:
                         break
-                    client_socket.send(data)
-                    count += 1 
-                    print( f'Enviado o segmento número {count}')
+                    else: 
+                        count += 1 
+                        data_size = len(data)
+                        acum_data_transfer += data_size
+                        print( f'Enviado o segmento número {count} com {data_size} bytes')
+                        print( f'Transferido {acum_data_transfer} de {file_stats.st_size} bytes - %.1f %%' % (100 * acum_data_transfer / file_stats.st_size))
+                        client_socket.send(data)
 
             # Envia uma mensagem final para o cliente 
             print('Envio concluído' )
